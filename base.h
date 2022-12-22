205b8d672312c7d4757f794edd4f24d23ad2bc2d
vsomani0 <vsomani0@gmail.com>
2022-12-22 09:59:18 -0800

    header for 2 player TTT

---
+++ base.h
@@ -0,0 +1,8 @@
+#include <iostream> 
+#include <vector>
+
+using namespace std;
+
+bool checkForWinner(const vector<vector<int>>& board);
+void getUserPos(const vector<vector<int>>& board);
+void displayBoard(const vector<vector<int>>& board);

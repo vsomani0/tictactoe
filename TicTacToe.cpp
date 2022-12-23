042653ae07415333c4b4b35a7111f0c1407d6b44
vsomani0 <vsomani0@gmail.com>
2022-12-23 12:04:21 -0800

    made it less restricted to allow the game to play beyond one TicTacToe, which is good for bigger boards. Also, fixed the check for win function and broke it into separate helper functions to save space.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,91 +1,156 @@
 #include "TicTacToe.h"
 
-TicTacToe::TicTacToe(int sides) {
+TicTacToe::TicTacToe(int sides)
+{
     std::vector<std::vector<char>> board(sides, std::vector<char>(sides, '-'));
     turnNumber = 1;
 }
-bool TicTacToe::boardFull() const{
-    if (turnNumber <= board.size()*board.size()) {
-        return false;
-    }
-    return true;
-}
-int TicTacToe::getTurnNumber() const {
-return turnNumber;
-}
-
-void TicTacToe::newGame(){
-    for (int y = 0; y < board.size(); y++)
+bool TicTacToe::boardFull() const
 {
-        for (int x = 0; x < board[y].size(); x++)
+    if (turnNumber <= board.size() * board.size())
     {
-            board[y][x] = '-';
-    }
-}
-turnNumber = 0;
-}
-bool TicTacToe::isValidInput(Coordinates location) const {
-    if (location.row < 0 || location.row > board.size()) {
-        return false;
-    }
-    if (location.col < 0 || location.col > board.size()) {
         return false;
     }
     return true;
 }
-bool TicTacToe::isWinnerDecided() const{
-char testChar{};
+int TicTacToe::getTurnNumber() const
+{
+    return turnNumber;
+}
 
-for (int i = 0; i < board.size(); ++i) {
-    testChar = board.at(i).at(0);
-    if (testChar == '-') {
-        continue;
-    }
-    for (int j = 0; j < board.at(0).size(); ++j) {
-        if (board.at(i).at(j) != testChar) {
-            break;
+void TicTacToe::newGame()
+{
+    for (int y = 0; y < board.size(); y++)
+    {
+        for (int x = 0; x < board[y].size(); x++)
+        {
+            board[y][x] = '-';
         }
-        return true;
     }
+    turnNumber = 0;
 }
-for (int j = 0; j < board.at(0).size(); ++j) {
-    testChar = board.at(0).at(j);
-    if (testChar == '-') {
-        continue;
+bool TicTacToe::isValidInput(Coordinates location) const
+{
+    if (location.row < 0 || location.row > board.size())
+    {
+        return false;
     }
-    for (int i = 0; i < board.size(); ++i) {
-        if (board.at(i).at(j) != testChar) {
-            break;
+    if (location.col < 0 || location.col > board.size())
+    {
+        return false;
+    }
+    return true;
+}
+int TicTacToe::numAltDiagonalWins() const {
+char testChar{};
+int sumWins{};
+    for (int i = 2; i < board.size(); ++i)
+    { 
+        for (int j = 0; j < board.at(0).size(); ++j)
+        {
+            testChar = board.at(i).at(j);
+            if (testChar == '-')
+            {
+            continue;
+            }
+            if (board.at(i-1).at(j+1) == testChar && board.at(i-2).at(j+2) == testChar)
+            {
+                sumWins++;
+            }
         }
-        return true;
     }
+    return sumWins;
 }
-// if ((board.at(0).at(0) == board.at(1).at(1)) && (board.at(1).at(1) == board.at(2).at(2)) && (board.at(0).at(0)!= '-')) {
-//         return true;
-// }
-// if ((board.at(2).at(0) == board.at(1).at(1)) && (board.at(1).at(1) == board.at(0).at(2)) && (board.at(2).at(0)!= '-')) {
-//         return true;
-//     }
-// for (int i = 0; i < board.size();++i) {
-//     if ((board.at(i).at(0) == board.at(i).at(1)) && (board.at(i).at(0) == board.at(i).at(2)) && (board.at(i).at(0)!= '-')) {
-//         return true;
-//     } 
-//     if ((board.at(0).at(i) == board.at(1).at(i)) && (board.at(0).at(i) == board.at(2).at(i)) && (board.at(i).at(0) != '-')) {
-//         return true;
-//     }
-//     }
-//     return false;
+int TicTacToe::numMainDiagonalWins() const {
+char testChar{};
+int sumWins{};
+    for (int i = 0; i < board.size() - 2; ++i)
+    { 
+        for (int j = 0; j < board.at(0).size() - 2; ++j)
+        {
+            testChar = board.at(i).at(j);
+            if (testChar == '-')
+            {
+            continue;
+            }
+            if (board.at(i+1).at(j+1) == testChar && board.at(i+2).at(j+2) == testChar)
+            {
+                sumWins++;
+            }
+        }
+    }
+    return sumWins;
 }
-void TicTacToe::display() const{
+int TicTacToe::numHorizontalWins() const {
+char testChar{};
+int sumWins{};
+    for (int i = 0; i < board.size() - 2; ++i)
+    { 
+        for (int j = 0; j < board.at(0).size(); ++j)
+        {
+            testChar = board.at(i).at(j);
+            if (testChar == '-')
+            {
+            continue;
+            }
+            if (board.at(i+1).at(j) == testChar && board.at(i+2).at(j) == testChar)
+            {
+                sumWins++;
+            }
+        }
+    }
+    return sumWins;
+}
+int TicTacToe::numVerticalWins() const {
+char testChar{};
+int sumWins{};
+    for (int i = 0; i < board.size(); ++i)
+    { 
+        for (int j = 0; j < board.at(0).size() - 2; ++j)
+        {
+            testChar = board.at(i).at(j);
+            if (testChar == '-')
+            {
+            continue;
+            }
+            if (board.at(i).at(j+1) == testChar && board.at(i).at(j+2) == testChar)
+            {
+                sumWins++;
+            }
+        }
+    }
+    return sumWins;
+}
+int TicTacToe::numWins() const
+{
+    return numVerticalWins() + numHorizontalWins() + numAltDiagonalWins() + numMainDiagonalWins();
+    // if ((board.at(0).at(0) == board.at(1).at(1)) && (board.at(1).at(1) == board.at(2).at(2)) && (board.at(0).at(0)!= '-')) {
+    //         return true;
+    // }
+    // if ((board.at(2).at(0) == board.at(1).at(1)) && (board.at(1).at(1) == board.at(0).at(2)) && (board.at(2).at(0)!= '-')) {
+    //         return true;
+    //     }
+    // for (int i = 0; i < board.size();++i) {
+    //     if ((board.at(i).at(0) == board.at(i).at(1)) && (board.at(i).at(0) == board.at(i).at(2)) && (board.at(i).at(0)!= '-')) {
+    //         return true;
+    //     }
+    //     if ((board.at(0).at(i) == board.at(1).at(i)) && (board.at(0).at(i) == board.at(2).at(i)) && (board.at(i).at(0) != '-')) {
+    //         return true;
+    //     }
+    //     }
+    //     return false;
+}
+void TicTacToe::display() const
+{
     unsigned int i{};
     unsigned int j{};
-    for (i = 0; i < board.size(); ++i) {
+    for (i = 0; i < board.size(); ++i)
+    {
         std::cout << std::endl;
-        for (int j = 0; j < board[i].size(); ++j) {
+        for (int j = 0; j < board[i].size(); ++j)
+        {
             std::cout << board[i][j];
         }
     }
     std::cout << std::endl;
 }
-
-

e349a03e5f01872e4f51e409aa9851efe484d605
vsomani0 <vsomani0@gmail.com>
2022-12-23 17:17:00 -0800

    finsihing touches

--- twoPlayerTTT.h
+++ twoPlayerTTT.h
@@ -2,9 +2,10 @@
 #define TWO_PLAYER_TTT_H
 #include "TicTacToe.h"
 #include <string>
-class twoPlayerTTT: public TicTacToe {
+class TwoPlayerTTT: public TicTacToe {
 public:
-twoPlayerTTT(int sides, std::string playerOneName, std::string playerTwoName);
+TwoPlayerTTT(int sides, std::string playerOneName, std::string playerTwoName);
+void newGame() override;
 std::string playerToMove() const;
 std::string getPlayerOneName() const;
 std::string getPlayerTwoName() const;
